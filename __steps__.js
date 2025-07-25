78a48cb9d81277ff2dc43dea9d2fa9f326d57bb0
subas-roy <subasroy46@gmail.com>
2025-07-25 07:13:38 +0600

    Basic project setup

---
+++ __steps__.js
@@ -0,0 +1,10 @@
+/**
+ * npm create vite@latest name-of-your-project -- --template react
+ * npm install react-router-dom
+ * npm install localforage match-sorter sort-by
+ * npm install -D tailwindcss@3 postcss autoprefixer
+ * npx tailwindcss init -p
+ * clear app.css
+ * add @tailwind base; @tailwind components; @tailwind utilities; to index.css
+ * npm i -D daisyui@latest
+ */
