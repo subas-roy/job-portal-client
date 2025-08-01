bd1f283fa57f0dc4f269d7426b61a73d40a5e199
subas-roy <subasroy46@gmail.com>
2025-08-01 15:19:35 +0600

    Create private route and auth redirect

---
+++ src/router/PrivateRoute.jsx
@@ -0,0 +1,16 @@
+import { useContext } from "react";
+import AuthContext from "../context/AuthContext/AuthContext";
+import { Navigate } from "react-router-dom";
+
+const PrivateRoute = ({children}) => {
+
+    const {user} = useContext(AuthContext);
+
+    if (user) {
+        return children;
+    }
+
+    return <Navigate to="/signin" />;
+};
+
+export default PrivateRoute;
