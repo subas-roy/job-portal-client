ff633a6c63ab119d87a029c941b27c86469d73a4
subas-roy <subasroy46@gmail.com>
2025-08-02 11:33:59 +0600

    Create a custom hook to get user info

--- src/pages/JobApply/JobApply.jsx
+++ src/pages/JobApply/JobApply.jsx
@@ -20,28 +20,20 @@ const JobApply = () => {
 
     return (
         <div className="hero bg-base-200 min-h-screen">
-            <div className="hero-content flex-col lg:flex-row-reverse">
-                <div className="text-center lg:text-left">
-                    <h1 className="text-5xl font-bold">Login now!</h1>
-                    <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
-                    </p>
-                </div>
-                <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-                    <div className="card-body">
-                        <form onSubmit={handleApplication}>
-                            <fieldset className="fieldset">
-                                <label className="label">LinkedIn URL</label>
-                                <input type="url" name="linkedIn" className="input" placeholder="LinkedIn URL" />
-                                <label className="label">Github URL</label>
-                                <input type="url" name="github" className="input" placeholder="Github URL" />
-                                <label className="label">Resume URL</label>
-                                <input type="url" name="resume" className="input" placeholder="Resume URL" />
-                                <button type="submit" className="btn btn-primary mt-4">Apply</button>
-                            </fieldset>
-                        </form>
-                    </div>
+            <div className="card bg-base-100 w-full shadow-2xl">
+                <div className="card-body">
+                    <h1 className="text-5xl font-bold">Apply Job and Good Luck!</h1>
+                    <form onSubmit={handleApplication}>
+                        <fieldset className="fieldset">
+                            <label className="label">LinkedIn URL</label>
+                            <input type="url" name="linkedIn" className="input w-full" placeholder="LinkedIn URL" />
+                            <label className="label">Github URL</label>
+                            <input type="url" name="github" className="input w-full" placeholder="Github URL" />
+                            <label className="label">Resume URL</label>
+                            <input type="url" name="resume" className="input w-full" placeholder="Resume URL" />
+                            <button type="submit" className="btn btn-primary mt-4">Apply</button>
+                        </fieldset>
+                    </form>
                 </div>
             </div>
         </div>
