dcb19d02547f521a48d050a7117d1c60254b4629
subas-roy <subasroy46@gmail.com>
2025-08-04 18:10:44 +0600

    Show my application in table

--- src/pages/MyApplications/MyApplications.jsx
+++ src/pages/MyApplications/MyApplications.jsx
@@ -12,8 +12,59 @@ const MyApplications = () => {
     }, [user.email]);
 
     return (
-        <div>
-            <h2 className='text-3xl'>My Applications {applications.length}</h2>
+        <div className="overflow-x-auto">
+            <table className="table">
+                {/* head */}
+                <thead>
+                    <tr>
+                        <th>
+                            <label>
+                                <input type="checkbox" className="checkbox" />
+                            </label>
+                        </th>
+                        <th>Name</th>
+                        <th>Job</th>
+                        <th>Favorite Color</th>
+                        <th></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* row 1 */}
+                    {
+                        applications.map(application => <tr key={application._id}>
+                            <th>
+                                <label>
+                                    <input type="checkbox" className="checkbox" />
+                                </label>
+                            </th>
+                            <td>
+                                <div className="flex items-center gap-3">
+                                    <div className="avatar">
+                                        <div className="mask mask-squircle h-12 w-12">
+                                            <img
+                                                src={application.company_logo}
+                                                alt="Company logo" />
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className="font-bold">{application.title}</div>
+                                        <div className="text-sm opacity-50">{application.location}</div>
+                                    </div>
+                                </div>
+                            </td>
+                            <td>
+                                Zemlak, Daniel and Leannon
+                                <br />
+                                <span className="badge badge-ghost badge-sm">Desktop Support Technician</span>
+                            </td>
+                            <td>Purple</td>
+                            <th>
+                                <button className="btn btn-ghost btn-xs">X</button>
+                            </th>
+                        </tr>)
+                    }
+                </tbody>
+            </table>
         </div>
     );
 };
